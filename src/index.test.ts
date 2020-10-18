00fd06f889e1b8643fabf5e819202f87e7c39692
Ben Stickley <bestickley@gmail.com>
2020-10-18 11:20:26 -0400

    feat: add exponential method to RVG class
    closes #2

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,8 @@
-import { getRandomNumber } from ".";
+import { RandVarGen } from ".";
 
-describe("getRandomNumber", () => {
-  test("gets value of type number", () => {
-    const num = getRandomNumber();
-    expect(typeof num).toBe("number");
+describe("exponential", () => {
+  test("correct value", () => {
+    const rvg = new RandVarGen();
+    expect(rvg.exponential(5)).toBe(-4.128057271167756);
   })
-  
-  test("gets value greater than 0", () => {
-    const num = getRandomNumber();
-    expect(num).toBeGreaterThan(0)
-  })
-  
-  test("gets value less than 1", () => {
-    const num = getRandomNumber();
-    expect(num).toBeLessThan(1)
-  })
-
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
-function getRandomNumber(): number {
-  return Math.random();
+function* lcg(modulus: number, a: number, c: number, seed: number) {
+  let value = seed;
+  while (true) {
+    value = (a * value + c) % modulus;
+    yield value;
+  }
 }
 
-export { getRandomNumber }
+interface RandVarGenConstructorParams {
+  customRandNumGen?: Generator<number, void, unknown>
+  lcgParams?: [number, number, number, number];
+  lcgSeed?: number;
+}
+
+export class RandVarGen {
+  constructor(params?: RandVarGenConstructorParams) {
+    if (params?.customRandNumGen) {
+      this.randNumGen = params.customRandNumGen;
+    } else if (params?.lcgParams) {
+      this.randNumGen = lcg(...params.lcgParams);
+    } else if (params?.lcgSeed) {
+      this.randNumGen = lcg(2**32, 1664525, 1013904223, params.lcgSeed);
+    }
+  }
+
+  // Default Random Number Generator is a Linear Congruential Generator
+  randNumGen: Generator<number, void, unknown> = lcg(2**32, 1664525, 1013904223, 123456789)
+
+  get random(): number {
+    const iterResult = this.randNumGen.next();
+    if (!iterResult.done) {
+      return iterResult.value;
+    } else {
+      throw new Error("Random Number Generator failed to yield a random number");
+    }
+  }
+
+  exponential(lambda: number) {
+    return (-1 / lambda) * Math.log(this.random);
+  }
+}
